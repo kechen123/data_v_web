5e625681de241afa085c8de6a6313bb6202207f4
e <k13772445714@163.com>
2021-12-06 14:19:04 +0800

    layout

---
+++ src/containers/body/left/components/pluginList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PlugList = (props) => {
+    return <div></div>
+}
+
+PlugList.propTypes = {}
+
+export default PlugList
---
+++ src/containers/body/left/components/pluginList1.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PluginList = (props) => {
+    return <div></div>
+}
+
+PluginList.propTypes = {}
+
+export default PluginList
