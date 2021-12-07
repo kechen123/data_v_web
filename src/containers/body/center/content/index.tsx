c296e35bab9b7d3ba798a6ea6c3953fa2def7e57
e <k13772445714@163.com>
2021-12-07 14:45:59 +0800

    formate

--- src/containers/body/center/content/index.tsx
+++ src/containers/body/center/content/index.tsx
@@ -4,11 +4,16 @@ import Background from '@components/background'
 import style from './index.module.less'
 
 const Content = (props) => {
-    return <>
-    <div className={style.ruler}><Ruler/></div>
-        <div className={style.background}><Background/></div>
+  return (
+    <>
+      <div className={style.ruler}>
+        <Ruler />
+      </div>
+      <div className={style.background}>
+        <Background />
+      </div>
     </>
+  )
 }
-
 
 export default Content
--- src/containers/body/center/screen/index.tsx
+++ src/containers/body/center/screen/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Drop from "@components/drop";
-import {useAppSelector} from "@storeApp/hooks";
-import {screen} from "@features/screenSlice";
+import Drop from '@components/drop'
+import { useAppSelector } from '@storeApp/hooks'
+import { screen } from '@features/screenSlice'
 
 const Screen = (props) => {
-    const { width,height,scale } = useAppSelector(screen)
-    return <Drop style={{width:width+'px',height:height+'px'}}>
-        <div>
-
-        </div>
+  const { width, height, scale } = useAppSelector(screen)
+  return (
+    <Drop style={{ width: width + 'px', height: height + 'px' }}>
+      <div></div>
     </Drop>
+  )
 }
-
 
 export default Screen
