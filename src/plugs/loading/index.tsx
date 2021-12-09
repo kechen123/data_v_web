b10b165208b2a3d4e55eee6386b7c809e80d5b31
e <k13772445714@163.com>
2021-12-09 13:31:29 +0800

    loading

--- src/plugs/loading/index.tsx
+++ src/plugs/loading/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Loading as DVLoading } from '@jiaminghi/data-view-react'
 import { SetWidget } from '@_data/Plugin'
-import style from './loading.module.less'
+import style from './index.module.less'
 
 const Loading = (props: SetWidget) => {
   const {
