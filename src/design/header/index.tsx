b0398075c966951f1a1bcec561c2f25f38ec0171
e <k13772445714@163.com>
2022-02-24 16:52:41 +0800

    保存预览

--- src/design/header/index.tsx
+++ src/design/header/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useAppSelector, useAppDispatch } from '@storeApp/hooks'
+import { screen } from '@features/screenSlice'
+import { widget } from '@features/widgetSlice'
+import style from './index.module.less'
 
 const Header = (props) => {
-    return <div></div>
+  const screenData = useAppSelector(screen)
+  const widgetData = useAppSelector(widget)
+  const yulan = () => {
+    console.log('预览>>>>')
+    console.log(screenData)
+    console.log(widgetData)
+  }
+  const save = () => {
+    console.log('保存')
+  }
+  return (
+    <div className={style.header}>
+      <div className={style.left}></div>
+      <div className={style.center}></div>
+      <div className={style.right}>
+        <div className={style.btn}>
+          <i className="icon iconfont icon-yulan " onClick={yulan}></i>
+        </div>
+        <div className={style.btn}>
+          <i className="icon iconfont icon-baocun " onClick={save}></i>
+        </div>
+      </div>
+    </div>
+  )
 }
-
-Header.propTypes = {}
 
 export default Header
--- src/design/setting/style/baseAttr/index.tsx
+++ src/design/setting/style/baseAttr/index.tsx
@@ -1,25 +1,30 @@
 import React, { useCallback, useEffect } from 'react'
 import { useSetState } from 'ahooks'
-import { Tooltip, Row, Col, Input, InputNumber } from 'antd'
+import { Form, Tooltip, Row, Col, Input, InputNumber } from 'antd'
 import eventBus from '@utils/eventBus'
 import style from './index.module.less'
 const margin = { marginBottom: '8px' }
+const layout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 18 },
+}
 interface State {
   left: number
   top: number
   width: number
   height: number
+  rotate: number
 }
 const BaseAttr = (props) => {
   const {
     id,
-    widget: { name, rect },
+    widget: { name, rect, rotate },
   } = props
 
   return (
     <div className={style.baseAttr}>
       <Info id={id} name={name} />
-      <Rect {...rect} />
+      <Rect {...{ ...rect, rotate: rotate }} />
     </div>
   )
 }
@@ -73,37 +78,44 @@ const Info = React.memo(
   }
 )
 
-const Rect = React.memo(({ left, top, width, height }: any) => {
+const Rect = React.memo(({ left, top, width, height, rotate }: any) => {
   const [frame, setFrame] = useSetState<State>({
     left: left,
     top: top,
     width: width,
     height: height,
+    rotate: rotate,
   })
   useEffect(() => {
     eventBus.addListener('widgetMove', (data: any) => {
       setFrame(data)
     })
   }, [])
+
   return (
-    <>
-      <Row justify="space-between" style={margin}>
-        <Col span={10}>
-          <InputNumber addonBefore="W" data-id="wiget-width" value={frame.width} />
-        </Col>
-        <Col span={10}>
-          <InputNumber addonBefore="H" value={frame.height} />
-        </Col>
-      </Row>
-      <Row justify="space-between" style={margin}>
-        <Col span={10}>
-          <InputNumber addonBefore="X" value={frame.left} />
-        </Col>
-        <Col span={10}>
-          <InputNumber addonBefore="Y" value={frame.top} />
-        </Col>
-      </Row>
-    </>
+    <Form {...layout}>
+      <Form.Item label="图表尺寸" style={{ marginBottom: 0 }}>
+        <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 6px)', marginRight: '6px' }}>
+          <InputNumber value={frame.width} onChange={(value) => {}} />
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 6px)', marginLeft: '6px' }}>
+          <InputNumber value={frame.height} onChange={(value) => {}} />
+        </Form.Item>
+      </Form.Item>
+      <Form.Item label="图表位置" style={{ marginBottom: 0 }}>
+        <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 6px)', marginRight: '6px' }}>
+          <InputNumber value={frame.left} onChange={(value) => {}} />
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 6px)', marginLeft: '6px' }}>
+          <InputNumber value={frame.top} onChange={(value) => {}} />
+        </Form.Item>
+      </Form.Item>
+      <Form.Item label="旋转角度" style={{ marginBottom: 0 }}>
+        <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 6px)', marginRight: '6px' }}>
+          <InputNumber value={frame.rotate} onChange={(value) => {}} />
+        </Form.Item>
+      </Form.Item>
+    </Form>
   )
 })
 
