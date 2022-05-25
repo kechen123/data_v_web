92a3dde8cfaace4d240a360e74fd845c3f602b95
e <k13772445714@163.com>
2022-05-25 14:11:29 +0800

    svg 优化

--- src/page/loading.tsx
+++ src/page/loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className={style.loading}>
       <svg width="200" height="200" viewBox="0 0 40 60">
-        <polygon className={style.triangle} fill="none" stroke="#fff" stroke-width="1" points="16,1 32,32 1,32" />
+        <polygon className={style.triangle} fill="none" stroke="#fff" strokeWidth="1" points="16,1 32,32 1,32" />
         <text className={style.text} x="0" y="45" fill="#fff">
           Loading...
         </text>
--- src/plugs/loading1/index.tsx
+++ src/plugs/loading1/index.tsx
@@ -23,17 +23,17 @@ const Loading = (props: WidgetObj) => {
 
         <circle className={style.loading_icon__background} r="95" fill="#f3f3f7"></circle>
 
-        <circle className={style.loading_icon__animated_stroke} clip-path="url(#loading-icon-clip-path)" r="100" fill="none" stroke-width="20px" stroke="#aaaaaa"></circle>
+        <circle className={style.loading_icon__animated_stroke} clip-path="url(#loading-icon-clip-path)" r="100" fill="none" strokeWidth="20px" stroke="#aaaaaa"></circle>
 
-        <circle className={style.loading_icon__static_stroke_section} clip-path="url(#loading-icon-clip-path)" r="100" fill="none" stroke-width="20px" stroke="#aaaaaa"></circle>
+        <circle className={style.loading_icon__static_stroke_section} clip-path="url(#loading-icon-clip-path)" r="100" fill="none" strokeWidth="20px" stroke="#aaaaaa"></circle>
 
         <path d="M -20,40 20,40 L 0,25 Z" stroke="none" fill="#aaaaaa" />
 
         <path d="M 0,0 C 0,0 -15,-10 -15,-20 H 15 C 15,-10 0,0 0,0 Z " stroke="none" fill="#aaaaaa" />
 
-        <path d="M 0,0 L 0,40" stroke-width="1px" stroke="#aaaaaa" fill="none" />
+        <path d="M 0,0 L 0,40" strokeWidth="1px" stroke="#aaaaaa" fill="none" />
 
-        <path d="M -25,-40 C -25,0 25,0 25,40 H -25 C -25,0 25,0 25,-40 Z " stroke-width="5px" stroke="#cacaca" fill="none" />
+        <path d="M -25,-40 C -25,0 25,0 25,40 H -25 C -25,0 25,0 25,-40 Z " strokeWidth="5px" stroke="#cacaca" fill="none" />
 
         <text y="140" fill="#777777" text-anchor="middle">
           &nbsp;&nbsp;Loading...
