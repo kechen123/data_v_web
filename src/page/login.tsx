fb97b452f19b5bfb1cca8150f94ede75734cfdfb
e <k13772445714@163.com>
2022-05-29 20:26:40 +0800

    login

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,20 +1,14 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Form, Input, Button, Checkbox } from 'antd'
+import { message } from 'antd'
 import { useRequest } from 'ahooks'
+import { postFetch } from '@utils/request'
+import '@assets/less/login.less'
 
-const login = (): Promise<string> => {
+const login = (username, password): Promise<string> => {
   return new Promise((resolve, reject) => {
-    fetch('http://127.0.0.1:5000/op/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: 'zhoutk',
-        password: '123456',
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => resolve(res.json()))
+    postFetch('/op/login', { username: username, password: password })
+      .then((res) => resolve(res))
       .catch(function (error) {
         reject(new Error('Failed to get username'))
       })
@@ -22,48 +16,64 @@ const login = (): Promise<string> => {
 }
 
 const Home = () => {
-  let navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   const { loading, run, runAsync } = useRequest(login, {
     manual: true,
+    debounceWait: 300,
+    throttleWait: 300,
     onSuccess: (result: any, params) => {
       if (result?.status === 200) {
         localStorage.setItem('userToken', result.token)
         window.location.reload()
+      } else {
+        message.error(result.message)
       }
     },
     onError: (error) => {
-      console.log('error', error)
+      message.error('网络错误')
     },
   })
 
-  const onFinish = (values: any) => {
-    run()
+  const onFinish = () => {
+    if (username && password && !loading) {
+      run(username, password)
+    }
   }
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo)
-  }
   return (
-    <div>
-      <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
-        <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+    <div className="login-box">
+      <h2>登录</h2>
+      <form>
+        <div className="user-box">
+          <input
+            type="text"
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+            required
+          />
+          <label>账号</label>
+        </div>
+        <div className="user-box">
+          <input
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            required
+          />
+          <label>密码</label>
+        </div>
+        <a href="#" onClick={onFinish}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          登录
+        </a>
+      </form>
     </div>
   )
 }
